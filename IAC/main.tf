79ef9dc4066467efcf7f028617869034915e5489
srihari-290899 <rssrihari1999@gmail.com>
2025-05-01 01:40:18 +0530

    Added Terraform Files

---
+++ IAC/main.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "demo-terraform-eks-state-s3-bucket-srihari-20250430-073619"
+    key            = "terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "terraform-eks-state-locks"
+    encrypt        = true
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+module "vpc" {
+  source = "git::https://github.com/srihari-2908/Ultimate-Devops-Project.git//IAC/Modules/VPC?ref=main"
+
+  vpc_cidr             = var.vpc_cidr
+  availability_zones   = var.availability_zones
+  private_subnet_cidrs = var.private_subnet_cidrs
+  public_subnet_cidrs  = var.public_subnet_cidrs
+  cluster_name         = var.cluster_name
+}
+
+module "eks" {
+  source = "git::https://github.com/srihari-2908/Ultimate-Devops-Project.git//IAC/Modules/EKS?ref=main"
+
+  cluster_name    = var.cluster_name
+  cluster_version = var.cluster_version
+  vpc_id          = module.vpc.vpc_id
+  subnet_ids      = module.vpc.private_subnet_ids
+  node_groups     = var.node_groups
+}
